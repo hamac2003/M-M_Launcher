8e78ac772a06bb4214270ae90442643df66ea09e
hamac2003 <41019535+hamac2003@users.noreply.github.com>
2020-05-15 10:46:20 -0400

    Add files via upload

--- M&M_Launcher_Final_Code/pi/mm_main_recognition.py
+++ M&M_Launcher_Final_Code/pi/mm_main_recognition.py
@@ -14,6 +14,29 @@ from loguru import logger
 from sinric import Sinric
 import argparse
 
+### References / Credits
+
+# Below are links to example code and/or libraries that I integrated into this script.
+"""
+[Facial Detection](https://www.pyimagesearch.com/2018/02/26/face-detection-with-opencv-and-deep-learning/)
+
+[Facial Landmark Detection](https://www.pyimagesearch.com/2017/04/17/real-time-facial-landmark-detection-opencv-python-dlib/)
+
+[Facial Recognition Library (and example code)](https://github.com/ageitgey/face_recognition)
+
+[Head Pose Estimation](https://www.learnopencv.com/head-pose-estimation-using-opencv-and-dlib/)
+
+[OpenCV Camera Calibration](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html)
+
+[Raspberry Pi - Arduino Serial Communication](https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/)
+
+[Alexa Raspberry Pi Communication Library](https://github.com/kakopappa/sinric)
+
+"""
+
+
+
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-t ", "--train", type=bool, default=False,
